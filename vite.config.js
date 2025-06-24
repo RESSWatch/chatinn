8040e9356c744d17655b2d14b017c3ecefb2b7e9
RESSWatch <sylvainress@hotmail.fr>
2025-06-24 21:35:57 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: ".",
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: "index.jsx",
+      name: "ChatInnWidget",
+      fileName: () => "widget.js",
+      formats: ["iife"],
+    },
+    minify: "esbuild",
+  },
+  plugins: [react()],
+  // Polyfills pour bundle IIFE
+  define: {
+    "process.env": {},
+    "process": "( { env: {} } )",
+  },
+});
