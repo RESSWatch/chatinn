c43561da807d98bf786a7a72d74688f72b34d80c
RESSWatch <sylvainress@hotmail.fr>
2025-06-24 23:49:09 +0200

    Delete frontend directory

--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-import ChatWidget from "./ChatWidget";
-
-const root = createRoot(document.getElementById("chatinn-root"));
-root.render(<ChatWidget />);
--- frontend/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import ChatWidget from "./ChatWidget";
-import "./theme.css";
-if(typeof window!=='undefined'&&!window.process){window.process={env:{}};}
-createRoot(document.getElementById("chatinn-root")).render(<ChatWidget />);
