3c73fa7b1d217bdf3608232b86c0fdb680422c07
RESSWatch <sylvainress@hotmail.fr>
2025-06-26 05:47:06 +0200

    Add files via upload

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ChatWidget from './ChatWidget.jsx';
-import './theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<ChatWidget />);
