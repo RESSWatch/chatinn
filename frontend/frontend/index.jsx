45f40a3370429c42e2422f2b3fbd9d70098947be
RESSWatch <sylvainress@hotmail.fr>
2025-06-26 05:20:46 +0200

    Delete frontend/frontend directory

--- frontend/frontend/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ChatWidget from './ChatWidget.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<ChatWidget />);
