44fe6dfe9b2310f169fafab973a801d632c194d5
RESSWatch <sylvainress@hotmail.fr>
2025-06-26 10:13:42 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,3 +8,15 @@ export default defineConfig({
     emptyOutDir: true
   }
 });
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import EnvironmentPlugin from 'vite-plugin-environment'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    // expose toutes les VITE_* à import.meta.env
+    EnvironmentPlugin('all', { prefix: 'VITE_' }),
+  ],
+})
